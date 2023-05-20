c10d178e526378a46c3085c9ee0ea2035f126bc8
Matthew Hogan <matthewghogan@pm.me>
2023-05-20 14:29:03 -0600

    Add model objects to init

--- model/__init__.py
+++ model/__init__.py
@@ -1,8 +1,13 @@
 """Model for the bank business"""
 
-from .account import Account
+from .account import Account, AccountStatusEnum
 from .branch import Branch
+from .business import Business
 from .customer import Customer, CustomerTypeEnum
 from .department import Department
 from .employee import Employee
 from .individual import Individual
+from .officer import Officer
+from .product import Product
+from .producttype import ProductType
+from .transaction import Transaction, TransactionTypeEnum

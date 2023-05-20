b2a1feb4d8520a95b7ce476f8f0115327998eaf4
Matthew Hogan <matthewghogan@pm.me>
2023-05-20 14:37:30 -0600

    Add repr and black

--- model/account.py
+++ model/account.py
@@ -17,36 +17,15 @@ class AccountStatusEnum(enum.Enum):
 
 
 class Account(Base):
-    """A customer in the schema
-    create table account
- (account_id integer unsigned not null auto_increment,
-  product_cd varchar(10) not null,
-  cust_id integer unsigned not null,
-  open_date date not null,
-  close_date date,
-  last_activity_date date,
-  status enum('ACTIVE','CLOSED','FROZEN'),
-  open_branch_id smallint unsigned,
-  open_emp_id smallint unsigned,
-  avail_balance float(10,2),
-  pending_balance float(10,2),
-  constraint fk_product_cd foreign key (product_cd)
-    references product (product_cd),
-  constraint fk_a_cust_id foreign key (cust_id)
-    references customer (cust_id),
-  constraint fk_a_branch_id foreign key (open_branch_id)
-    references branch (branch_id),
-  constraint fk_a_emp_id foreign key (open_emp_id)
-    references employee (emp_id),
-  constraint pk_account primary key (account_id)
- );
-    """
+    """A customer in the schema"""
 
     __tablename__: Final[str] = "account"
 
     account_id: Mapped[int] = mapped_column(primary_key=True)
 
-    product_cd: Mapped[str] = mapped_column(ForeignKey("product.product_cd"), nullable=False)
+    product_cd: Mapped[str] = mapped_column(
+        ForeignKey("product.product_cd"), nullable=False
+    )
 
     cust_id: Mapped[int] = mapped_column(ForeignKey("customer.cust_id"), nullable=False)
 
@@ -59,12 +38,41 @@ class Account(Base):
     last_activity_date: Mapped[Optional[date]] = mapped_column(Date, nullable=True)
     """Last activity date of the account, nullable means use open date"""
 
-    status: Mapped[Optional[str]] = mapped_column(Enum(AccountStatusEnum), nullable=True)
+    status: Mapped[Optional[str]] = mapped_column(
+        Enum(AccountStatusEnum), nullable=True
+    )
 
-    open_emp_id: Mapped[Optional[int]] = mapped_column(ForeignKey("employee.emp_id"), nullable=True)
+    open_emp_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("employee.emp_id"), nullable=True
+    )
 
-    open_branch_id: Mapped[Optional[int]] = mapped_column(ForeignKey("branch.branch_id"), nullable=True)
+    open_branch_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("branch.branch_id"), nullable=True
+    )
 
-    avail_balance: Mapped[Optional[float]] = mapped_column(Float(precision=10), nullable=True)
+    avail_balance: Mapped[Optional[float]] = mapped_column(
+        Float(precision=10), nullable=True
+    )
 
-    pending_balance: Mapped[Optional[float]] = mapped_column(Float(precision=10), nullable=True)
+    pending_balance: Mapped[Optional[float]] = mapped_column(
+        Float(precision=10), nullable=True
+    )
+
+    def __repr__(self) -> str:
+        return (
+            "Account(account_id=%d, product_cd=%s, cust_id=%d, open_date=%s, close_date=%s"
+            ", last_activity_date=%s, status=%s, open_emp_id=%s, open_branch_id=%s"
+            ", avail_balance=%s, pending_balance=%s)"
+        ) % (
+            self.account_id,
+            self.product_cd,
+            self.cust_id,
+            self.open_date.isoformat(),
+            self.close_date.isoformat() if self.close_date is not None else "",
+            self.last_activity_date.isoformat() if self.last_activity_date is not None else "",
+            self.status if self.status is not None else "",
+            str(self.open_emp_id) if self.open_emp_id is not None else "",
+            str(self.open_branch_id) if self.open_branch_id is not None else "",
+            str(round(self.avail_balance, 2)) if self.avail_balance is not None else "",
+            str(round(self.pending_balance, 2)) if self.pending_balance is not None else "",
+        )

1a91813b7cd2768984f67002a86fa6fd817ed616
Matthew Hogan <matthewghogan@pm.me>
2023-05-20 13:16:39 -0600

    Add init declarations

---
+++ model/__init__.py
@@ -0,0 +1,8 @@
+"""Model for the bank business"""
+
+from .account import Account
+from .branch import Branch
+from .customer import Customer, CustomerTypeEnum
+from .department import Department
+from .employee import Employee
+from .individual import Individual

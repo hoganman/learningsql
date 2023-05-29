3b1a3b7f8cc126f2309ea20986ef948ac43a8268
Matthew Hogan <matthewghogan@pm.me>
2023-05-28 18:50:49 -0600

    Add documentation to account

--- model/account.py
+++ model/account.py
@@ -22,12 +22,15 @@ class Account(Base):
     __tablename__: Final[str] = "account"
 
     account_id: Mapped[int] = mapped_column(primary_key=True)
+    """Account ID, primary key"""
 
     product_cd: Mapped[str] = mapped_column(
         ForeignKey("product.product_cd"), nullable=False
     )
+    """The product code, foreign key to a product's code, nullable"""
 
     cust_id: Mapped[int] = mapped_column(ForeignKey("customer.cust_id"), nullable=False)
+    """Customer ID, foreign key to a customer's ID"""
 
     open_date: Mapped[date] = mapped_column(Date, nullable=False)
     """Open date of the account, non-nullable"""
@@ -41,26 +44,33 @@ class Account(Base):
     status: Mapped[Optional[AccountStatusEnum]] = mapped_column(
         Enum(AccountStatusEnum), nullable=True
     )
+    """Status of the account, nullable if undefined"""
 
     open_emp_id: Mapped[Optional[int]] = mapped_column(
         ForeignKey("employee.emp_id"), nullable=True
     )
+    """Employee's ID who opened the account, foreign key to an employee's ID"""
 
     open_branch_id: Mapped[Optional[int]] = mapped_column(
         ForeignKey("branch.branch_id"), nullable=True
     )
+    """Branch's ID where the account was opened, foreign key to a branch's ID"""
 
     avail_balance: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    """Available balance of the account, nullable if no activity"""
 
     pending_balance: Mapped[Optional[float]] = mapped_column(Float, nullable=True)
+    """"Pending balance of the account, nullable if no activity"""
 
     account_customer: Mapped["Customer"] = relationship(
         "Customer", back_populates="customer_accounts"
     )
+    """A pointer to the Customer of the account"""
 
     account_product: Mapped["Product"] = relationship(
         "Product", back_populates="product_accounts"
     )
+    """A pointer to the account's Product"""
 
     def __repr__(self) -> str:
         return (
